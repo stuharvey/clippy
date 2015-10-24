53f8d80c01e0149bb529a30f19d90dca9ac15ed2
stuharvey <stu@vt.edu>
2015-10-24 10:38:06 -0400

    Detect newly copied text in the background

--- desktop/clippy-desk.js
+++ desktop/clippy-desk.js
@@ -1,29 +1,56 @@
+// declare clipboard accessors
 var copyPaste = require('copy-paste');
 // copy(text, [callback])
 var copy = copyPaste.copy; // copies @param text into the clipboard
 // paste([callback])
 var paste = copyPaste.paste; // pastes the most recently copied item
 
+// start the clipboard service when the page renders
 $(document).ready(function() {
-    $("#currentPBText").text("Your most recent clipboard item:\n" + paste());
+    var clippy_desk = new ClippyDesk();
 });
 
-$("#copyfield")
-    .keyup(function() {
-        // display the input as it is edited
-        var inputText = $(this).val();
-        $("#in").text("You typed: " + inputText);
-    });
-
-$("#copyfield")
-    .keypress(function(e) {
-        // if enter is pressed, test copy+paste
-        if (e.which == 13) {
-            testCopyPaste($(this).val());
+var ClippyDesk = function() { 
+    this.lastCopied = paste();
+    $("#currentPBText").text("Your most recent clipboard item:\n" +
+                             this.lastCopied);
+    $("#copyfield")
+        .keyup(function() {
+            // display the input as it is edited
+            var inputText = $(this).val();
+            $("#in").text("You typed: " + inputText);
         }
-    });
+    );
 
-var testCopyPaste = function(text) {
+    var that = this;
+    $("#copyfield")
+        .keypress(function(e) {
+            // if enter is pressed, test copy+paste
+            if (e.which == 13) {
+                that.testCopyPaste($(this).val());
+            }
+        }
+    );
+
+    // checks to see if anything new has been copied
+    setInterval(that.checkClipboard, 1500, that);
+}
+
+ClippyDesk.prototype.checkClipboard = function(that) {
+    var current = paste();
+    var previous = that.lastCopied;
+    if (previous !== current) {
+        that.updateClippyboard(current);
+    }
+}
+
+ClippyDesk.prototype.updateClippyboard = function(newlyCopied) {
+    this.lastCopied = newlyCopied;
+    $("#paste").text("Copied \"" + newlyCopied + "\"");
+    // will send to server here
+}
+
+ClippyDesk.prototype.testCopyPaste = function(text) {
     copyPaste.copy(text); // should copy to the system clipboard if everything works
     $("#paste").text("Copied \"" + paste() + "\"");
 }
