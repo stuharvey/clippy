127bd9c9e8affb1f0a1ad75e96b4f0aa14fb0d22
stuharvey <stu@vt.edu>
2015-10-28 21:29:07 -0400

    Add global keybindings ctrl+alt+c and ctrl+alt+v

--- desktop/clippy-desk.js
+++ desktop/clippy-desk.js
@@ -5,10 +5,66 @@ var https = require('https');
 var copy = copyPaste.copy; // copies @param text into the clipboard
 // paste([callback])
 var paste = copyPaste.paste; // pastes the most recently copied item
+var gui = require('nw.gui');
+
+// --------- UI
 
 // start the clipboard service when the page renders
 $(document).ready(function() {
     var clippy_desk = new ClippyDesk();
+
+    var win = gui.Window.get();
+    win.focus(); // brings the application window into focus
+
+    // --------- KEYBOARD SHORTCUTS
+    var copyOption = { 
+        key: "Ctrl+Alt+C", 
+        active: function() { 
+            console.log("Global copy shortcut: " + this.key + " active.");
+            // TODO: call system clipboard copy, emit to server
+        },
+        failed: function(msg) {
+            // :(, fail to register the |key| or couldn't parse the |key|.
+            console.log(msg);
+        }
+    };
+
+    var pasteOption = { 
+        key: "Ctrl+Alt+V", 
+        active: function() { 
+            // ctrl+alt+v
+            console.log("Global paste shortcut: " + this.key + " active.");
+            clippy_desk.getFromServer('admin');
+            // wait a few seconds, then ctrl+v
+        },
+        failed: function(msg) {
+            // :(, fail to register the |key| or couldn't parse the |key|.
+            console.log(msg);
+        }
+    };
+
+    var closeOption = {
+        key: "Ctrl+Q",
+        active: function() {
+            console.log("Ctrl+Q to quit");
+        },
+        failed: function(msg) {
+            console.log("Can't quit with Ctrl+Q");
+        }
+    }
+
+    var copyShortcut = new gui.Shortcut(copyOption);
+    var pasteShortcut = new gui.Shortcut(pasteOption);
+    var closeShortcut = new gui.Shortcut(closeOption);
+
+    gui.App.registerGlobalHotKey(copyShortcut);
+    gui.App.registerGlobalHotKey(pasteShortcut);
+
+    win.on('close', function() {
+        gui.App.unregisterGlobalHotKey(copyShortcut);
+        gui.App.unregisterGlobalHotKey(pasteShortcut);  
+        gui.App.closeAllWindows();      
+    });
 });
 
 var ClippyDesk = function() {
@@ -39,9 +95,11 @@ var ClippyDesk = function() {
             str += chunk;
         });
         res.on('end', function() {
-            console.log("Server sent: " + str);
+            var clipboard = JSON.parse(str);
+            console.log("User's clipboard is:\n" + clipboard.toString());
             $("#response").text("User's clipboard is: ");
-            $("#clipboard").text(JSON.parse(str));
+            $("#clipboard").text(clipboard);
+            copy(clipboard[clipboard.length - 1]);
         });
     };
 
@@ -57,6 +115,7 @@ var ClippyDesk = function() {
     }
 
     this.lastCopied = paste();
+    this.copiedFromServer = paste();
 
     // checks to see if anything new has been copied
     setInterval(that.checkClipboard, 1500, that);
